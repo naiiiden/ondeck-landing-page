fac19e20fb81485294fe26c73eafde6458a036c9
naiden <na4ko359@gmail.com>
2022-04-11 14:36:08 +0300

    nav icon changes to X after open/change nav border radius

--- script/script.js
+++ script/script.js
@@ -1,3 +1,8 @@
 document.querySelector(".hamburger").addEventListener("click", () => {
     document.querySelector(".products_container").classList.toggle("show");
+    if (document.querySelector(".menu-icon").getAttribute("src") === "Assets/Menu.svg") {
+        document.querySelector(".menu-icon").setAttribute("src", "Assets/Close.svg");
+    } else {
+        document.querySelector(".menu-icon").setAttribute("src", "Assets/Menu.svg");
+    }
 });
