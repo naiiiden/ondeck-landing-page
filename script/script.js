658590597035b6ad0cd616d484abe2fdb2c5961d
naiden <na4ko359@gmail.com>
2022-04-12 14:58:33 +0300

    toggle products code refactor

--- script/script.js
+++ script/script.js
@@ -1,18 +1,24 @@
-document.querySelector(".hamburger").addEventListener("click", () => {
-    document.querySelector(".products_container").classList.toggle("show");
-    if (document.querySelector(".menu-icon").getAttribute("src") === "Assets/Menu.svg") {
-        document.querySelector(".menu-icon").setAttribute("src", "Assets/Close.svg");
-    } else {
-        document.querySelector(".menu-icon").setAttribute("src", "Assets/Menu.svg");
-    }
-});
+const openProductsList = document.querySelectorAll(".toggle_products");
+openProductsList.forEach(button => {
+    button.addEventListener("click", () => {
+        document.querySelector(".products_container").classList.toggle("show");
+        
+        const mobileQuery = window.matchMedia("(max-width: 1023px)");
+        if (mobileQuery.matches) {
+            if (document.querySelector(".menu-icon").getAttribute("src") === "Assets/Menu.svg") {
+                document.querySelector(".menu-icon").setAttribute("src", "Assets/Close.svg");
+            } else {
+                document.querySelector(".menu-icon").setAttribute("src", "Assets/Menu.svg");
+            }
+        }
 
-document.querySelector(".toggle_products").addEventListener("click", () => {
-    document.querySelector(".products_container").classList.toggle("show");
-});
-
-window.addEventListener("click", (e) => {
-    if (!e.target.matches('.toggle_products')) {
-      document.querySelector(".products_container").classList.remove("show");
-    }
+        const desktopQuery = window.matchMedia("(min-width: 1024px)");
+        if (desktopQuery.matches) {
+           window.addEventListener("click", (e) => {
+                if (!e.target.matches('.toggle_products')) {
+                    document.querySelector(".products_container").classList.remove("show");
+                }
+            }); 
+        }
+    });
 });
